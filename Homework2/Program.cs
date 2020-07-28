ff891c097ee232f98c8acc8337bafa81d7fe9edf
Karol <karol_w49@interia.pl>
2020-07-28 19:11:11 +0200

    update full project2

---
+++ Homework2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var result = false;
+                var trialCounter = 1;
+                var randomNumber = new Random();
+                var guessNumber = randomNumber.Next(0, 100);
+                Console.WriteLine("Komputer wylosował liczbę z zakresu 0-100.\n");
+                Console.WriteLine("Spróbuj zgadnąć. Wprowadź liczbę:\n");
+
+                while (!result)
+                {
+                    if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                        throw new Exception("Podana wartość jest nieprawidłowa!");
+                    if (userNumber < 0 || userNumber > 100)
+                        throw new Exception("Podana przez Ciebie liczba jest spoza zakresu!\n");
+                    if (userNumber == guessNumber)
+                    {
+                        Console.WriteLine("Brawo! Odgadłeś liczbę wylosowaną przez komputer w {0} próbie.", trialCounter);
+                        result = true;
+                    }
+                    else if (userNumber > guessNumber)
+                    {
+                        Console.WriteLine("\nPodałeś większą liczbę. Spróbuj ponownie!");
+                        result = false;
+                        trialCounter++;
+                    }
+                    else if (userNumber < guessNumber)
+                    {
+                        Console.WriteLine("\nPodałeś mniejszą liczbę. Spróbuj ponownie!");
+                        result = false;
+                        trialCounter++;
+                    }
+                }
+                Console.WriteLine("\nDziękuję za udział w grze!\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nPrzykro mi, ale to koniec gry.");
+                Console.WriteLine("\nNaciśnj dowolony klawisz, aby zakończyć grę i wyjść.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
